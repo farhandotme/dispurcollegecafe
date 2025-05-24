85a57c19b2828b05499c52b6e48cc2ec5d98bd1b
Mafijur <lilmoficodes@gmail.com>
2025-05-24 16:01:06 +0530

    development phase

--- components/LoginWrapper.tsx
+++ components/LoginWrapper.tsx
@@ -2,16 +2,27 @@
 import LoginUserSchema, { LoginUserProps } from "@/validators/LoginUser";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form"
+import { Button } from "./ui/button";
 const LoginWrapper = () =>{
-const {register, handleSubmit, formState : {isSubmitting, isValid}} = 
+const {register, handleSubmit, formState : {isSubmitting, isValid, errors}} = 
 useForm<LoginUserProps>({resolver : zodResolver(LoginUserSchema)});
-const handleLogin =  async () =>{
-
+const handleLogin =  async (data : LoginUserProps) =>{
+console.log(data);
 }
 return (
-    <main className="shadow-[0px_0px_1px_1.2px]">
-        <div>
-            <form action=""></form>
+    <main className="h-screen flex items-center justify-center">
+        <div className="shadow-[0px_0px_30px_1.2px_white] w-96 rounded-md">
+            <h1 className="text-2xl font-semibold text-center mt-2">Login to continue</h1>
+            <form onSubmit={handleSubmit(handleLogin)} className="p-5 flex flex-col rounded-md h-fit gap-y-3">
+             <label htmlFor="email">Email</label>
+            <input {...register("email")}
+             type="text" id="email" className="border border-white/10 outline-none rounded-md p-1 px-3 bg-transparent" 
+            placeholder="abc@example.com"/>
+            <label htmlFor="password">Password</label>
+            <input className="border border-white/10 outline-none rounded-md p-1 px-3 bg-transparent"  
+            type="password" {...register("password", {minLength : 8 , maxLength : 20})} id="password"/>
+            <Button>Login</Button>
+            </form>
         </div>
     </main>
 )
