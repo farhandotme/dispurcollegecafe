90c9124c2303724a79ca7b02f575fa07ed3a7fff
Mafijur <lilmoficodes@gmail.com>
2025-05-24 10:28:45 +0530

    development phase

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import withMetaData from "@/components/withMetaData";
 import HomeWrapper from "@/components/HomeWrapper";
 const {metadata, default : Home} = withMetaData({
@@ -7,13 +6,3 @@ const {metadata, default : Home} = withMetaData({
 }, HomeWrapper)
 export {metadata}
 export default Home
-=======
-
-export default function Home() {
-  return (
-    <div>
-      Hello Disour college
-    </div>
-  );
-}
->>>>>>> f99e36db2c73c875311aa931f87368e59a501137
